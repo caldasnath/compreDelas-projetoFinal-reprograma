9910794ef5c7bba2681e75bdf0817284c4c5bea5
nathalia <nacaldas@hotmail.com>
2022-07-24 17:26:30 -0300

    Conecta banco de dados

--- src/database/mongooseConnect.js
+++ src/database/mongooseConnect.js
@@ -1,11 +1,8 @@
 const DATABASE_URI = process.env.DATABASE_URI
-
-
 const mongoose = require('mongoose')
 
 const connect = async () => {
   try {
-    
     await mongoose.connect(DATABASE_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
